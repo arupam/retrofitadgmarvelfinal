0e2065a82def5f1f34af1dd000cd977b6851f84d
arupam <sahaarupam37@gmail.com>
2020-05-08 17:36:26 +0530

    .

--- app/src/main/java/com/example/retrofitadgmarvel/MainActivity.java
+++ app/src/main/java/com/example/retrofitadgmarvel/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.retrofitadgmarvel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -16,10 +18,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView mHeroDetails = findViewById(R.id.textView);
+        mHeroDetails.setMovementMethod(new ScrollingMovementMethod());
 
         // building retrofit object
         Retrofit retrofit = new Retrofit.Builder()
@@ -38,12 +44,18 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<herodetails>> call, Response<List<herodetails>> response) {
 
                 List<herodetails> heros= response.body(); // all details re now in this
+                String herodetails="";
 
+                //To verify
                 for(herodetails h: heros){
-                    Log.d("name",h.getName());
-                    Log.d("realname",h.getRealname());
+                   // Log.d("name",h.getName());
+                    //Log.d("realname",h.getRealname());
+                    herodetails+= "Hero: "+h.getName()+"\nReal Name: "+h.getRealname()+"\nTeam: "+h.getTeam()
+                            +"\nFirst Appearance: "+h.getFirstappearance()+"\nCreated by: "+h.getCreatedby()
+                            +"\nPublisher: "+h.getPublisher()+"\nImageUrl: "+h.getImageurl()+"\nbio: "+h.getBio()
+                            +"\n---------------------------------------\n";
 
-                }
+                }mHeroDetails.setText(herodetails);
 
             }
 
